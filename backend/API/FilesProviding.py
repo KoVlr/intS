5d69220302fb7b7e0f57c16a1b0c6900d954e5d3
KoVlr <kovlrfw@gmail.com>
2023-02-15 21:55:35 +0300

    Changed public files providing

--- backend/API/FilesProviding.py
+++ backend/API/FilesProviding.py
@@ -10,10 +10,6 @@ FilesProviding_router = APIRouter(
 def root():
     return FileResponse("public/index.html")
 
-@FilesProviding_router.get("/public/{file_name}")
-def get_file(file_name: str):
-    return FileResponse(f"public/{file_name}")
-
 @FilesProviding_router.get("/{path:path}")
 def to_root(path: str):
     return FileResponse("public/index.html")
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from backend.API.auth import auth_router
 from backend.API.test import test_router
 from backend.API.FilesProviding import FilesProviding_router
+from fastapi.staticfiles import StaticFiles
 from backend.database.db_models import Base
 from backend.database.database import engine
 
@@ -12,6 +13,7 @@ app = FastAPI()
 
 app.include_router(auth_router)
 app.include_router(test_router)
+app.mount("/public", StaticFiles(directory="public"), name="public")
 app.include_router(FilesProviding_router)
 
 if __name__ == '__main__':
